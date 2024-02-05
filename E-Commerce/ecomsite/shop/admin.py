c3db1a2cd85d7affb146127636e938319a070810
Tymo Verhaegen <r0878860@student.thomasmore.be>
2024-02-05 17:46:43 +0100

    admin panel customization

--- E-Commerce/ecomsite/shop/admin.py
+++ E-Commerce/ecomsite/shop/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Product, Order
 
+admin.site.site_header = "E-Commerce Admin Page"
+admin.site.site_title = "Tymo's Store"
+
+admin.site.index_title = "Manage"
+
+class ProductAdmin(admin.ModelAdmin):
+    # Create a function to change category to default
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category='default')
+        
+    # Change action name
+    change_category_to_default.short_description = 'Default Category'
+    list_display = ['title', 'price', 'discount_price', 'category']
+    search_fields = ['title', 'category']
+    list_filter = ['category']
+    list_editable = ['price', 'discount_price']
+    actions = ['change_category_to_default']
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
+
